b772dcde129bd2082c852b102d09d18397037c80
disk0401 <c01123489e@edu.teu.ac.jp>
2019-08-10 14:20:53 +0900

    実行スケジューラとワーカーを実装

--- app.py
+++ app.py
@@ -4,6 +4,33 @@ from logging import DEBUG, INFO, Formatter, StreamHandler, getLogger
 from config import slack_token
 
 
+def scheduler(interval_time, func, wait=True):
+    """
+    ワーカーを管理する。
+    waitが
+    ・Trueの場合 ：前回の実行が終了していない場合は、再度指定時間待つ。
+    ・Falseの場合：前回の実行が終了していない場合は、新規スレッドで同時に実行する
+    """
+    base_time = time.time()
+    next_time = 0
+    while True:
+        t = threading.Thread(target=func, args=(interval_time,))
+        t.start()
+        if wait:
+            t.join()
+        next_time = ((base_time - time.time()) % interval_time) or interval_time
+        time.sleep(next_time)
+
+
+def worker(interval_time):
+    """
+    ワーカー。
+    実際に実行される関数
+    """
+    logger.info("[start] worker")
+    logger.info("[end] worker")
+
+
 if __name__ == '__main__':
     logger = getLogger(__name__)
     handler_format = Formatter('[%(asctime)s][%(name)s][%(levelname)s]-%(message)s')
@@ -22,3 +49,6 @@ if __name__ == '__main__':
     logger.setLevel(DEBUG)
     logger.propagate = False
     logger.info("Start-UP")
+    interval_tm = 15
+    logger.info("[interval_time] " + str(interval_tm) + "s")
+    scheduler(interval_tm, worker, wait=True)
